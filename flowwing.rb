3cbc521a05814b5c3e9bf5b7b27f1cef97daa98d
Kushagra Rathore <71135665+kushagra1212@users.noreply.github.com>
2024-03-17 01:01:22 +0530

    Update flowwing.rb

--- flowwing.rb
+++ flowwing.rb
@@ -2,7 +2,7 @@ class Flowwing < Formula
   desc "A Programming language that uniquely combines dynamic and static typing paradigms"
   homepage "https://github.com/kushagra1212/Flow-Wing"
   url "https://github.com/kushagra1212/Flow-Wing/releases/download/v0.0.1-alpha/FlowWing-0.0.1-alpha.tar.gz"
-  sha256 "c5f7d7d3b7e4489cadb44844aaa0e97809497ddbd134658ab61771355f65d226"
+  sha256 "71571d013194ee693d39834f7901434ad3672bd3ec4fc045b9cbc9c64acda35d"
   license "FlowWing"
 
   def install
