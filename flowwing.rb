f3b1944f912913a9c1b83689c9d57e6260953d2c
Kushagra Rathore <71135665+kushagra1212@users.noreply.github.com>
2024-09-29 23:57:21 +0530

    Update flowwing.rb

--- flowwing.rb
+++ flowwing.rb
@@ -1,13 +1,19 @@
 class Flowwing < Formula
-  desc "A Programming language that uniquely combines dynamic and static typing paradigms"
+  desc "Sculpting logic with the elegance of flow."
   homepage "https://github.com/kushagra1212/Flow-Wing"
-  url "https://github.com/kushagra1212/Flow-Wing/releases/download/v0.0.1-alpha/FlowWing-0.0.1-alpha.tar.gz"
-  sha256 "08528a5d324f922d71440cc705d5ba48c44249dd8ef79bee6e20fa549879b06c"
-  license "FlowWing"
+  url "https://github.com/kushagra1212/Flow-Wing/releases/download/untagged-75a44737a4ca391388cc/FlowWing-0.0.1-alpha-m1.tar.gz"
+  sha256 "4f2197dbb491875ef145dbba102e6b3a8c2dd5718179bce873ac8154fd241551"
+  license "FlowWing Contribution License v1.0"
 
   def install
     bin.install "bin/FlowWing"
-    (lib/"FlowWing").install "lib/libbuilt_in_module.a"
+    (lib/"FlowWing/lib").install "lib/libbuilt_in_module.a"
+    (lib/"FlowWing/lib").install "lib/libflowwing_string.a"
+    (lib/"FlowWing/lib").install "lib/libraylib.a"
+    (lib/"FlowWing/lib").install "lib/libflowwing_vector.so"
+    (lib/"FlowWing/lib").install "lib/libflowwing_map.so"
+
+    (lib/"FlowWing/lib/modules").install "lib/modules"
   end
 
   test do
