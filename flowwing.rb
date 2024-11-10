d005c8ffd0a4492ecaf99cf7552c492e51f9d3d7
Kushagra Rathore <71135665+kushagra1212@users.noreply.github.com>
2024-11-11 01:21:05 +0530

    Update flowwing.rb

--- flowwing.rb
+++ flowwing.rb
@@ -2,7 +2,7 @@ class Flowwing < Formula
   desc "Sculpting logic with the elegance of flow."
   homepage "https://github.com/kushagra1212/Flow-Wing"
   url "https://github.com/kushagra1212/Flow-Wing/releases/download/v0.0.1-alpha.1/FlowWing-0.0.1-mac-arm-64.tar.gz"
-  sha256 "382fa3fcaa39e5b80915670de248dbc7314eb404e6aa1cdb0c58fed542079253"
+  sha256 "b794c32a45c85ef3912cdc4ee480d849bb46565e076550f4a39038171f65fe93"
   license "FlowWing Contribution License v1.0"
 
   def install
@@ -10,7 +10,6 @@ class Flowwing < Formula
     bin.install "bin/FlowWing"
     bin.install "bin/FlowWing-jit"
     bin.install "bin/FlowWing-repl"
-    bin.install "bin/fg-link"
 
     lib.install "lib/libbuilt_in_module.a"
     lib.install "lib/libflowwing_string.a"
